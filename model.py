e3b1e445fd15ac5a68f559731c728771599d7ebc
yashbonde <bonde.yash97@gmail.com>
2020-12-04 03:25:01 +0530

    add Weight Decay Optimizer

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ from torch.utils.data import IterableDataset, DataLoader, Dataset
 from torch.utils.tensorboard import SummaryWriter
 
 from transformers import GPT2Model, GPT2Config as ModelConfig
-from transformers.models.gpt2.modeling_gpt2 import MLP, Attention
+from transformers.models.gpt2.modeling_gpt2 import MLP, Attention, Conv1D
 
 # ---- helper function ----- #
 def set_seed(seed):
@@ -101,6 +101,8 @@ class BetaChess(nn.Module):
             nn.Tanh()
         ])
 
+        self.apply(self._init_weights)
+
     def forward(self, input_ids, value_targets = None, loss = None, **gptkwargs):
         x = self.body(input_ids, return_dict = True, **gptkwargs)
         logits = self.policy_head(x.last_hidden_state)
@@ -120,6 +122,58 @@ class BetaChess(nn.Module):
 
             out = (logits, values, (loss, loss_policy, loss_value))
         return out
+
+    def _init_weights(self, module):
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            module.weight.data.normal_(mean=0.0, std=0.02)
+            if isinstance(module, nn.Linear) and module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+
+    def configure_optimizers(self, train_config):
+        """
+        This long function is unfortunately doing something very simple and is being very defensive:
+        We are separating out all parameters of the model into two buckets: those that will experience
+        weight decay for regularization and those that won't (biases, and layernorm/embedding weights).
+        We are then returning the PyTorch optimizer object.
+        """
+        # separate out all parameters to those that will and won't experience regularizing weight decay
+        decay = set()
+        no_decay = set()
+        whitelist_weight_modules = (torch.nn.Linear, Conv1D, Attention)
+        blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
+        for mn, m in self.named_modules():
+            for pn, p in m.named_parameters():
+                fpn = '%s.%s' % (mn, pn) if mn else pn # full param name
+                
+                pn_type = pn.split(".")[-1]
+                if pn_type == 'bias':
+                    # all biases will not be decayed
+                    no_decay.add(fpn)
+                elif pn_type == 'weight' and isinstance(m, whitelist_weight_modules):
+                    # weights of whitelist modules will be weight decayed
+                    decay.add(fpn)
+                elif pn_type == 'weight' and isinstance(m, blacklist_weight_modules):
+                    # weights of blacklist modules will NOT be weight decayed
+                    no_decay.add(fpn)
+
+        # validate that we considered every parameter
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        inter_params = decay & no_decay
+        union_params = decay | no_decay
+        assert len(inter_params) == 0, "parameters %s made it into both decay/no_decay sets!" % (str(inter_params), )
+        assert len(param_dict.keys() - union_params) == 0, "parameters %s were not separated into either decay/no_decay set!" \
+                                                    % (str(param_dict.keys() - union_params), )
+
+        # create the pytorch optimizer object
+        optim_groups = [
+            {"params": [param_dict[pn] for pn in sorted(list(decay))], "weight_decay": train_config.weight_decay},
+            {"params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay": 0.0},
+        ]
+        optimizer = torch.optim.AdamW(optim_groups, lr=train_config.lr, betas=train_config.betas)
+        return optimizer
 
 ################################################
 ####### Trainer ################################
@@ -156,11 +210,12 @@ class Trainer:
         wandb.watch(model)
         
         # create step functions
-        optimizer = torch.optim.Adam(
-            model.parameters(),
-            lr = config.lr,
-            betas = config.betas
-        )
+        # optimizer = torch.optim.Adam(
+        #     model.parameters(),
+        #     lr = config.lr,
+        #     betas = config.betas
+        # )
+        optimizer = model.configure_optimizers(config) # get AdamW optimiser
 
         # setup correct scheduler
         if config.scheduler == "CosineAnnealingWarmRestarts":
@@ -325,6 +380,7 @@ class TrainerConfig:
     patience = -1
     test_every = None
     scheduler = None
+    weight_decay = 0.1
 
     def __init__(self, **kwargs):
         self.attrs = [
